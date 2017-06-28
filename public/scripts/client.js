0bdd955197642b110dbeeb5f5a7541ccc2bd9163
Erin Black <erinblackdesign@gmail.com>
2017-06-28 15:14:34 -0500

    create html pages for all landing pages on the site and put them in the config to route them

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,8 +7,16 @@ myApp.config(function($routeProvider){
   $routeProvider.when('/', {
     templateUrl: 'views/partials/login.html',
     controller:'MyProfile as mp'
+  }).when('/register', {
+    templateUrl: 'views/partials/register.html',
   }).when('/admin', {
     templateUrl: 'views/partials/admin.html',
+  }).when('/book', {
+    templateUrl: 'views/partials/book.html',
+  }).when('/edit-profile', {
+    templateUrl: 'views/partials/editProfile.html',
+  }).when('/main', {
+    templateUrl: 'views/partials/main.html',
   }); //end submit
 }); //end config
 
---
+++ public/scripts/services/loginService.js
@@ -0,0 +1,5 @@
+myApp.service('LoginService', function($http){
+console.log('inside LoginService');
+var ls = this;
+
+}); //end LoginService
