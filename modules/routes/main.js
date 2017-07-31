84be38b8e7aabe725024425b90ca8f79fe224539
Erin Black <erinblackdesign@gmail.com>
2017-07-30 21:16:10 -0500

    Added support for heroku.

--- modules/routes/main.js
+++ modules/routes/main.js
@@ -2,18 +2,9 @@ var express  = require('express');
 var router = express.Router();
 var path = require('path');
 var bodyParser = require('body-parser');
-var pg = require('pg');
 
 
-//config to database
-var config = {
-database: 'bookaholics',
-host: 'localhost',
-port: 5432,
-max: 50
-};
-
-var pool = new pg.Pool(config);
+var pool = require('../pool.js');
 
 router.use( bodyParser.urlencoded( { extended: true } ) );
 router.use( bodyParser.json() );
--- modules/routes/register.js
+++ modules/routes/register.js
@@ -3,18 +3,8 @@ var router = express.Router();
 var path = require('path');
 var bcrypt = require( 'bcrypt' );
 var bodyParser = require('body-parser');
-var pg = require('pg');
 
-
-//config to database
-var config = {
-database: 'bookaholics',
-host: 'localhost',
-port: 5432,
-max: 50
-};
-
-var pool = new pg.Pool(config);
+var pool = require('../pool.js');
 
 router.use( bodyParser.urlencoded( { extended: true } ) );
 router.use( bodyParser.json() );
